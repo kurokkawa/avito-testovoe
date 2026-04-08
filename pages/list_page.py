9eab9777a938408cead051ee844863b18f314f2a
kurokkawa <saoniki123@gmail.com>
2026-04-08 17:23:26 +0300

    Create list_page.py

---
+++ pages/list_page.py
@@ -0,0 +1,48 @@
+import re
+from .base_page import BasePage
+
+
+class ListPage(BasePage):
+
+    MIN_PRICE = 'input[placeholder*="Мин"]'
+    MAX_PRICE = 'input[placeholder*="Макс"]'
+    PRICE_ITEMS = '[data-testid="price"]'
+    ITEM_CARDS = '[data-testid="item"]'
+    URGENT_BADGE = 'text=Срочно'
+
+    def set_min_price(self, value):
+        self.fill(self.MIN_PRICE, value)
+        self.wait_for_update()
+
+    def set_max_price(self, value):
+        self.fill(self.MAX_PRICE, value)
+        self.wait_for_update()
+
+    def wait_for_update(self):
+        # базовое ожидание (можно заменить на loader/network)
+        self.page.wait_for_timeout(800)
+
+    def get_prices(self):
+        texts = self.page.locator(self.PRICE_ITEMS).all_text_contents()
+        prices = []
+
+        for text in texts:
+            clean = re.sub(r"[^\d]", "", text)
+            if clean:
+                prices.append(int(clean))
+
+        return prices
+
+    def toggle_urgent(self):
+        self.click('input[type="checkbox"]')
+        self.wait_for_update()
+
+    def get_items(self):
+        return self.page.locator(self.ITEM_CARDS)
+
+    def has_urgent_only(self):
+        items = self.get_items()
+        for i in range(items.count()):
+            if not items.nth(i).locator(self.URGENT_BADGE).is_visible():
+                return False
+        return True
