8d787d7a0dc0b990b602d3c5548cd09f382a576c
kurokkawa <saoniki123@gmail.com>
2026-04-08 17:57:06 +0300

    Update test_list.py

--- tests/test_list.py
+++ tests/test_list.py
@@ -7,29 +7,29 @@ def test_min_price_filter(page):
     page_obj = ListPage(page)
     page_obj.open(BASE_URL)
 
-    page_obj.set_min_price("100")
+    page_obj.set_min_price("1000")
     prices = page_obj.get_prices()
 
     assert len(prices) > 0, "Нет объявлений"
-    assert all(p >= 100 for p in prices)
+    assert all(p >= 1000 for p in prices)
 
 
 def test_max_price_filter(page):
     page_obj = ListPage(page)
     page_obj.open(BASE_URL)
 
-    page_obj.set_max_price("500")
+    page_obj.set_max_price("10000")
     prices = page_obj.get_prices()
 
     assert len(prices) > 0
-    assert all(p <= 500 for p in prices)
+    assert all(p <= 10000 for p in prices)
 
 
 def test_sort_price(page):
     page_obj = ListPage(page)
     page_obj.open(BASE_URL)
 
-    page.select_option("select", "asc")
+    page.select_option('select[class="_filters__select_1iunh_21"]', 'asc')
     page.wait_for_timeout(800)
 
     prices = page_obj.get_prices()
